69f29022d4d5cba428570615e87f2606dc4dbe8d
JerryWebLee <jerryweblee@outlook.com>
2025-08-24 16:00:04 +0800

    feat: 添加 Prettier 配置到 lint-staged

---
+++ utils/test-format.js
@@ -0,0 +1,11 @@
+const test = {
+  name: 'test',
+  value: 123,
+  items: [1, 2, 3, 4, 5],
+}
+
+function formatTest() {
+  return `${test.name} ${test.value}`
+}
+
+module.exports = { test, formatTest }
